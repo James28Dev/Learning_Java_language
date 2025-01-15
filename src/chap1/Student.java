af3795e11b9ed7514995d9a301bcac7544b4afef
James28Dev <james28.dev@gmail.com>
2025-01-16 00:32:36 +0700

    Update Student.java

--- src/chap1/Student.java
+++ src/chap1/Student.java
@@ -1,30 +1,63 @@
-
 package chap1;
 
 public class Student {
-    String studentId, studentName, faculty, major;
-    
-    void enrollment(String studenId) {
+
+    protected String studentId, studentName, faculty, major;
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public void enrollment(String studenId) {
         System.out.println("enrollment()");
     }
-    
-    void payment(String studentId) {
-        System.out.println("payment()");
+
+    public void payment(String studentId) {
+        System.out.println("Moujay");
     }
-    
-    void addCourse(String studenId) {
+
+    public void addCourse(String studenId) {
         System.out.println("addCourse()");
     }
-    
-    void dropCourse(String studenId) {
+
+    public void dropCourse(String studenId) {
         System.out.println("dropCourse()");
     }
-    void printAttribute() {
-        System.out.println("================================");
+
+    public void printAttribute() {
+        System.out.println("\n================================");
         System.out.println("ID : " + studentId);
         System.out.println("Name : " + studentName);
         System.out.println("Faculty : " + faculty);
         System.out.println("Major : " + major);
         System.out.println("================================");
     }
-}
+} //end
