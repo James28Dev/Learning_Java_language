10612ab62e9fa7581ffdc51fd6871033b1057b14
James28Dev <james28.dev@gmail.com>
2025-01-17 02:10:17 +0700

    Update Vehicle.java

--- src/chap1/Vehicle.java
+++ src/chap1/Vehicle.java
@@ -1,56 +1,56 @@
-
 package chap1;
 
 public class Vehicle {
+
     String brand, series, color;
     double price;
-    
-    void move(){
+
+    void move() {
         System.out.println("move()");
-    }
-    
+    } //end move class
+
     boolean turnRight() {
         System.out.println("turnRight()");
         return true;
-    }
-    
+    } //end turnRight class
+
     boolean turnLeft() {
         System.out.println("turnLeft()");
         return true;
-    }
-    
+    } //end turnLeft class
+
     void doBreak() {
         System.out.println("break()");
-    }
-    
+    } //end doBreak class
+
     void start() {
         System.out.println("start()");
-    }
-    
+    } //end start class
+
     void stop() {
         System.out.println("stop()");
-    }
-    
+    } //end stop class
+
     void showDetail() {
         System.out.println("Brand :" + brand);
         System.out.println("Series :" + series);
         System.out.println("Color :" + color);
         System.out.println("Price :" + price);
-    }
-    
+    } //end showDetail class
+
     public static void main(String[] args) {
-        Vehicle car = new Vehicle();
+        Vehicle car = new Vehicle(); //create car object
         System.out.println("car object " + car);
         car.brand = "Honda";
         car.series = "CRV";
         car.color = "Pink";
         car.price = 1500000;
-        car.start();
-        car.move();
-        car.turnLeft();
-        car.turnRight();
-        car.doBreak();
-        car.stop();
-        car.showDetail();
-    }
-}
+        car.start(); //call start
+        car.move(); //call move
+        car.turnLeft(); //call turnLeft
+        car.turnRight(); //call turnRight
+        car.doBreak(); //call doBreak
+        car.stop(); //call stop
+        car.showDetail(); //call showDetail
+    } //end main class
+} //end Vehicle class
