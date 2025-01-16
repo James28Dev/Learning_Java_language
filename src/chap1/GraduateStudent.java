91610c38dc6b6d72c433db1a3812fe6b3707faf5
James28Dev <james28.dev@gmail.com>
2025-01-17 02:10:08 +0700

    Update GraduateStudent.java

--- src/chap1/GraduateStudent.java
+++ src/chap1/GraduateStudent.java
@@ -6,37 +6,37 @@ public class GraduateStudent extends Student {
 
     public String getStudylevel() {
         return studyLevel;
-    }
+    } //end getStudylevel class
 
     public void setStudylevel(String studylevel) {
         this.studyLevel = studylevel;
-    }
+    } //end setStudylevel class
 
     public String getThesisAdver() {
         return thesisAdver;
-    }
+    } //end getThesisAdver class
 
     public void setThesisAdver(String thesisAdver) {
         this.thesisAdver = thesisAdver;
-    }
+    } //end setThesisAdver class
 
     public void oralExamination(String studenid) {
         System.out.println("oralExamination()");
-    }
+    } //end oralExamination class
 
     public void thesisExamination(String studenid) {
         System.out.println("thesisExamination()");
-    }
+    } //end thesisExamination class
 
     @Override
     public void payment(String studentId) {
         System.out.println("Pay by Credit");
-    }
+    } //end payment class
 
     @Override
     public void printAttribute() {
         super.printAttribute();
         System.out.println("StudyLevel : " + studyLevel);
         System.out.println("ThesisAdver : " + thesisAdver);
-    }
-} //end
+    } //end printAttribute class
+} //end GraduateStudent class
