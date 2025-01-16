829157556b8a0c54f509f5f54b2ce386fe9de535
James28Dev <james28.dev@gmail.com>
2025-01-17 02:10:31 +0700

    Update MultiplicationTable.java

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -3,24 +3,22 @@ public class MultiplicationTable {
 
     static void printHead() {
         System.out.println("----------------------------------------------------------------------------------------------------");
-        for (int i = 1; i <= 12; i++) {
-            System.out.print("\t" + i);
-        }
+        for (int i = 1; i <= 12; i++)
+            System.out.print("\t" + i); //end for loops
         System.out.println("\n----------------------------------------------------------------------------------------------------");
-    }
+    } //end printHead class
 
     static void printDetails() {
         for (int i = 2; i <= 12; i++) {
             System.out.print(i + "\t");
-            for (int j = 1; j <= 12; j++) {
-                System.out.print(i * j + "\t");
-            }
+            for (int j = 1; j <= 12; j++)
+                System.out.print(i * j + "\t"); //end for loops
             System.out.println("\n");
-        }
-    }
+        } //end for loops
+    } //end printDetails class
 
     public static void main(String[] args) {
-        printHead();
-        printDetails();
-    }
-}
+        printHead(); //call printHead class
+        printDetails(); //call printDetails class
+    } //end main class
+} //end MultiplicationTable class
