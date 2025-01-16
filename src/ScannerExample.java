71899ffc5c296501de133e493650fc23b0ad79f4
James28Dev <james28.dev@gmail.com>
2025-01-17 02:10:34 +0700

    Update ScannerExample.java

--- src/ScannerExample.java
+++ src/ScannerExample.java
@@ -7,23 +7,23 @@ public class ScannerExample {
         Scanner sc = new Scanner(System.in);
         
         System.out.print("Enter name : ");
-        String name = sc.next();
+        String name = sc.next(); //get name
         
         System.out.print("Enter surname : ");
-        String surname = sc.next();
+        String surname = sc.next(); //get surname
         
         System.out.print("Enter age : ");
-        int age = sc.nextInt();
+        int age = sc.nextInt(); //get age
         
         System.out.print("Enter gpa : ");
-        double gpa = sc.nextDouble();
+        double gpa = sc.nextDouble(); //get gpa
         
         System.out.println("name = " + name);
         System.out.println("surname = " + surname);
         System.out.println("age = " + age);
         System.out.println("gpa = " + gpa);
         
-        String strAge = HelloWorld.checkAge(age);
+        String strAge = HelloWorld.checkAge(age); //call checkAge class in HelloWorld.java
         System.out.println("AGE = " + strAge);
-    }
-}
+    } //end main class
+} //end ScannerExample class
