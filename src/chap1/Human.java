90884fa30ffb2dd44f660891704f0a3d8d95ac40
James28Dev <james28.dev@gmail.com>
2025-01-17 02:10:12 +0700

    Update Human.java

--- src/chap1/Human.java
+++ src/chap1/Human.java
@@ -1,38 +1,37 @@
-
 package chap1;
 
 public class Human {
+
     String name, gender;
     int age;
-    
+
     void introduce() {
         System.out.println("introduce()");
-    }
-    
+    } //end introduce class
+
     void sleep() {
         System.out.println("sleep()");
-    }
-    
+    } //end sleep class
+
     void speak() {
         System.out.println("speak()");
-    }
-    
+    } //end speak class
+
     void showDetail() {
         System.out.println("Name : " + name);
         System.out.println("Gender : " + gender);
         System.out.println("Age : " + age);
-        
-    }
-    
+    } //end showDetail class
+
     public static void main(String[] args) {
-        Human man = new Human();
+        Human man = new Human(); //create man object
         System.out.println("man object : " + man);
         man.name = "james";
         man.gender = "Male";
         man.age = 21;
-        man.showDetail();
-        man.introduce();
-        man.sleep();
-        man.speak();
-    }
-}
+        man.showDetail(); //call showDetail class
+        man.introduce(); //call introduce class
+        man.sleep(); //call sleep class
+        man.speak(); //call speak class
+    } //end main class
+} //end Human class
