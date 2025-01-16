c3d94b7b0b37d4a94e50c6a56b4c8c900d04a4ae
James28Dev <james28.dev@gmail.com>
2025-01-17 02:10:15 +0700

    Update Student.java

--- src/chap1/Student.java
+++ src/chap1/Student.java
@@ -6,51 +6,51 @@ public class Student {
 
     public String getStudentId() {
         return studentId;
-    }
+    } //end getStudentId class
 
     public void setStudentId(String studentId) {
         this.studentId = studentId;
-    }
+    } //end setStudentId class
 
     public String getStudentName() {
         return studentName;
-    }
+    } //end getStudentName class
 
     public void setStudentName(String studentName) {
         this.studentName = studentName;
-    }
+    } //end setStudentName class
 
     public String getFaculty() {
         return faculty;
-    }
+    } //end getFaculty class
 
     public void setFaculty(String faculty) {
         this.faculty = faculty;
-    }
+    } //end setFaculty class
 
     public String getMajor() {
         return major;
-    }
+    } //end getMajor class
 
     public void setMajor(String major) {
         this.major = major;
-    }
+    } //end setMajor class
 
     public void enrollment(String studenId) {
         System.out.println("enrollment()");
-    }
+    } //end enrollment class
 
     public void payment(String studentId) {
         System.out.println("Moujay");
-    }
+    } //end payment class
 
     public void addCourse(String studenId) {
         System.out.println("addCourse()");
-    }
+    } //end addCourse class
 
     public void dropCourse(String studenId) {
         System.out.println("dropCourse()");
-    }
+    } //end dropCourse class
 
     public void printAttribute() {
         System.out.println("\n================================");
@@ -59,5 +59,5 @@ public class Student {
         System.out.println("Faculty : " + faculty);
         System.out.println("Major : " + major);
         System.out.println("================================");
-    }
+    } //end printAttribute class
 } //end
