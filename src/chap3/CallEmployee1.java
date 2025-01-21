552eb8a2cedc4c148e3be676df8a2e313b6c34fd
James28Dev <james28.dev@gmail.com>
2025-01-21 23:22:28 +0700

    Update CallEmployee1.java

--- src/chap3/CallEmployee1.java
+++ src/chap3/CallEmployee1.java
@@ -1,12 +1,14 @@
 package chap3;
+
 public class CallEmployee1 {
+
     public static void main(String[] args) {
         // call static
-        System.out.println("ID : "+Employee1.id);
+        System.out.println("ID : " + Employee1.id);
         Employee1.work();
         // call non-static
         Employee1 em = new Employee1();
-        System.out.println("Name : "+em.name);
-        System.out.println("Age : "+em.age);
-   }
+        System.out.println("Name : " + em.name);
+        System.out.println("Age : " + em.age);
+    }
 }//end
