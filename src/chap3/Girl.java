74ee0cea925ee5136f09b0b136a20353f22ca379
James28Dev <james28.dev@gmail.com>
2025-01-21 19:56:25 +0700

    Create Girl.java

---
+++ src/chap3/Girl.java
@@ -0,0 +1,15 @@
+package chap3;
+public class Girl {
+    String name;
+    String phone;
+
+    public Girl(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+    
+    void print(){
+        System.out.println("Name : "+name);
+        System.out.println("PhoneNo : "+phone);
+    }
+}
