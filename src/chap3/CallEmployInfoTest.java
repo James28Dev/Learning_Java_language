c84bcb7c5894f41df6995c1acf3cee86cbf65c85
James28Dev <james28.dev@gmail.com>
2025-01-21 23:22:30 +0700

    Update CallEmployInfoTest.java

--- src/chap3/CallEmployInfoTest.java
+++ src/chap3/CallEmployInfoTest.java
@@ -1,8 +1,9 @@
 package chap3;
 
 public class CallEmployInfoTest {
+
     public static void main(String[] args) {
-        double newSalary =25000 + EmployInfoTest.incSalary(25000, EmployInfoTest.rate);
-        System.out.println("เงินเดือนใหม่ คือ "+newSalary);
+        double newSalary = 25000 + EmployInfoTest.incSalary(25000, EmployInfoTest.rate);
+        System.out.println("เงินเดือนใหม่ คือ " + newSalary);
     }
 }//end
