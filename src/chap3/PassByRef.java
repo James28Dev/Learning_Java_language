aa7658fbd934d3a41880f42093192c33587c078c
James28Dev <james28.dev@gmail.com>
2025-01-21 23:22:42 +0700

    Update PassByRef.java

--- src/chap3/PassByRef.java
+++ src/chap3/PassByRef.java
@@ -1,12 +1,15 @@
 package chap3;
+
 public class PassByRef {
-    static void changeGirl(Girl g){
+
+    static void changeGirl(Girl g) {
         g.phone = "099999999";
     }
+
     public static void main(String[] args) {
-        Girl gl = new Girl("Ney", "0826591281");
+        Girl gl = new Girl("James", "0842664168");
         gl.print();
-        
+
         changeGirl(gl);
         gl.print();
     }
