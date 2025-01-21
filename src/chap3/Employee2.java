f8ace3d9ff5439c4de9b8320a7861d87d7525dcb
James28Dev <james28.dev@gmail.com>
2025-01-21 23:22:35 +0700

    Update Employee2.java

--- src/chap3/Employee2.java
+++ src/chap3/Employee2.java
@@ -1,5 +1,7 @@
 package chap3;
+
 public class Employee2 {
+
     int id;
     String name;
     int age;
@@ -16,43 +18,43 @@ public class Employee2 {
         this.id = id;
         this.name = name;
     }
+
     public Employee2(int id, String name, int age) {
         this.id = id;
         this.name = name;
         this.age = age;
     }
-    
-    
-    static void work(){
+
+    static void work() {
         System.out.println("work()");
     }
-    
-    void getSalary(){
+
+    void getSalary() {
         System.out.println("getSalary()");
     }
-    
-    void printAttribute(){
+
+    void printAttribute() {
         System.out.println("-----------------------------");
-        System.out.println("ID : "+id);
-        System.out.println("Name : "+name);
-        System.out.println("Age : "+age);
+        System.out.println("ID : " + id);
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
         System.out.println("-----------------------------");
     }
-    
+
     public static void main(String[] args) {
         // call static
 //        work();
         // call non-static
         Employee2 em = new Employee2();
         em.printAttribute();
-        Employee2 em2 = new Employee2(4001);
+        Employee2 em2 = new Employee2(4011);
         em2.printAttribute();
-        Employee2 em3 = new Employee2(4001, "Ney");
+        Employee2 em3 = new Employee2(4011, "James");
         em3.printAttribute();
-        Employee2 em4 = new Employee2(4001, "Ney",18 );
+        Employee2 em4 = new Employee2(4011, "James", 21);
         em4.printAttribute();
 //        System.out.println("ID : "+em.id);
 //        System.out.println("Name : "+em.name);
 //        System.out.println("Age : "+em.age);
-   }
+    }
 }//end
